5c66968c525f87a534b8d0e4e6c4bf5506aaff1c
hitchhooker <tommi@niemi.lol>
2024-01-24 07:59:47 +0700

    enrich amount of shown data

--- api/beefy.mjs
+++ api/beefy.mjs
@@ -53,95 +53,156 @@ async function fetchIdentities(addresses, providerUrl) {
     const provider = new WsProvider(providerUrl);
     const api = await ApiPromise.create({ provider });
 
-    const identities = await Promise.all(addresses.map(async (address) => {
+    async function fetchIdentity(address) {
         const identity = await api.query.identity.identityOf(address);
+
         if (identity.isSome) {
-            const { display, email, riot } = identity.unwrap().info;
+            const { judgements, deposit, info } = identity.unwrap();
+            const { display, legal, web, riot, email, pgpFingerprint, image, twitter } = info;
+
             return {
                 address,
+                judgements: judgements.map(j => [j[0].toString(), j[1].toString()]),
+                deposit: deposit.toString(),
                 displayName: display.isRaw ? hexToString(display.asRaw.toHex()) : '',
+                legal: legal.isRaw ? hexToString(legal.asRaw.toHex()) : '',
+                web: web.isRaw ? hexToString(web.asRaw.toHex()) : '',
+                matrix: riot.isRaw ? hexToString(riot.asRaw.toHex()) : '',
                 email: email.isRaw ? hexToString(email.asRaw.toHex()) : '',
-                matrix: riot.isRaw ? hexToString(riot.asRaw.toHex()) : ''
+                pgpFingerprint: pgpFingerprint ? hexToString(pgpFingerprint.toHex()) : '',
+                image: image.isRaw ? hexToString(image.asRaw.toHex()) : '',
+                twitter: twitter.isRaw ? hexToString(twitter.asRaw.toHex()) : ''
+            };
+        } else {
+            // Check if it's a sub-identity and fetch the parent identity
+            const superOf = await api.query.identity.superOf(address);
+            if (superOf.isSome) {
+                const parentAddress = superOf.unwrap()[0].toString();
+                return fetchIdentity(parentAddress); // Recursively fetch parent identity
+            }
+
+            return {
+                address,
+                judgements: [],
+                deposit: '',
+                displayName: '',
+                legal: '',
+                web: '',
+                matrix: '',
+                email: '',
+                pgpFingerprint: '',
+                image: '',
+                twitter: ''
             };
         }
-        return { address, displayName: '', email: '', matrix: '' };
-    }));
+    }
+
+    let allIdentities = [];
+    for (const address of addresses) {
+        const identity = await fetchIdentity(address);
+        allIdentities.push(identity);
+    }
 
     await api.disconnect();
-    return identities;
+    return allIdentities;
 }
 
 async function fetchDataAndUpdateCache(cache) {
-    const validators1kvMap = await fetch1KVValidators();
-    const dummyValidators = await fetchData('https://kusama-staging.w3f.community/validators/beefy/dummy')
-        .then(data => data.map(item => item.address));
+  const validators1kvMap = await fetch1KVValidators();
+  const dummyValidators = await fetchData('https://kusama-staging.w3f.community/validators/beefy/dummy')
+    .then(data => data.map(item => item.address));
 
-    const identities = await fetchIdentities(dummyValidators, 'wss://rpc.ibp.network/kusama');
+  const identities = await fetchIdentities(dummyValidators, 'wss://rpc.ibp.network/kusama');
 
-    const candidates1kv = [];
-    const candidatesnon1kv = [];
+  const candidates1kv = [];
+  const candidatesnon1kv = [];
 
-    identities.forEach(identity => {
-        const { address, displayName, email, matrix } = identity;
-        const is1kv = validators1kvMap.hasOwnProperty(address);
+  identities.forEach(identity => {
+    const {
+      address,
+      displayName,
+      email,
+      matrix,
+      judgements,
+      deposit,
+      legal,
+      web,
+      pgfingerprint,
+      image,
+      twitter
+    } = identity;
 
-        const validatorData = {
-            address,
-            name: displayName || (is1kv ? validators1kvMap[address].name : ''),
-            email: email || (is1kv ? validators1kvMap[address].email : ''),
-            matrix: matrix || (is1kv ? validators1kvMap[address].matrix : '')
-        };
+    const is1kv = validators1kvMap.hasOwnProperty(address);
 
-        if (is1kv) {
-            candidates1kv.push(validatorData);
-        } else {
-            candidatesnon1kv.push(validatorData);
-        }
-    });
+    let validatorData = {
+      address,
+      is1kv: is1kv
+    };
 
-    const output = { candidates1kv, candidatesnon1kv };
-    cache.setCachedData('main', output);
-    return output;
+    // Add fields only if they have data
+    if (displayName) validatorData.identity = displayName;
+    if (is1kv && validators1kvMap[address].name) validatorData.nodeName = validators1kvMap[address].name;
+    if (email) validatorData.email = email;
+    if (matrix) validatorData.matrix = matrix;
+    if (judgements && judgements.length > 0) validatorData.judgements = judgements;
+    if (deposit) validatorData.deposit = deposit;
+    if (legal) validatorData.legal = legal;
+    if (web) validatorData.web = web;
+    if (pgfingerprint) validatorData.pgfingerprint = pgfingerprint;
+    if (image) validatorData.image = image;
+    if (twitter) validatorData.twitter = twitter;
+
+    if (is1kv) {
+      candidates1kv.push(validatorData);
+    } else {
+      candidatesnon1kv.push(validatorData);
+    }
+  });
+
+  // const output = { candidates1kv, candidatesnon1kv };
+  const output = { validator: candidates1kv.concat(candidatesnon1kv) };
+  cache.setCachedData('main', output);
+  return output;
 }
 
 async function fetchBeefyDataAndUpdateCache(cache) {
-    const beefyDataUrl = 'https://kusama.w3f.community/validators/beefy';
-    const beefyData = await fetchData(beefyDataUrl);
+  const beefyDataUrl = 'https://kusama.w3f.community/validators/beefy';
+  const beefyData = await fetchData(beefyDataUrl);
 
-    cache.setCachedData('beefyStatus', beefyData);
-    return beefyData;
+  cache.setCachedData('beefyStatus', beefyData);
+  return beefyData;
 }
 
 async function status(cache) {
-    if (cache.isStale('beefyStatus')) {
-        console.log("Beefy cache is stale. Fetching new data...");
-        return await fetchBeefyDataAndUpdateCache(cache);
-    }
+  if (cache.isStale('beefyStatus')) {
+    console.log("Beefy cache is stale. Fetching new data...");
+    return await fetchBeefyDataAndUpdateCache(cache);
+  }
 
-    console.log("Returning cached Beefy data");
-    return cache.getCachedData('beefyStatus');
+  console.log("Returning cached Beefy data");
+  return cache.getCachedData('beefyStatus');
 }
 
 async function main(cache) {
-    if (cache.isStale('main')) {
-        console.log("Cache is stale. Fetching new data...");
-        // Wait for cache update if no data is available
-        if (!cache.getCachedData('main')) {
-            await fetchDataAndUpdateCache(cache);
-        } else {
-            // Update cache in background if stale data is available
-            fetchDataAndUpdateCache(cache);
-        }
-    }
-
-    const cachedData = cache.getCachedData('main');
-    if (cachedData) {
-        console.log("Returning cached data");
-        return cachedData;
+  if (cache.isStale('main')) {
+    console.log("Cache is stale. Fetching new data...");
+    // Wait for cache update if no data is available
+    if (!cache.getCachedData('main')) {
+      await fetchDataAndUpdateCache(cache);
     } else {
-        console.log("No cached data available. Fetching new data...");
-        return await fetchDataAndUpdateCache(cache);
+      // Update cache in background if stale data is available
+      fetchDataAndUpdateCache(cache);
     }
+  }
+
+  const cachedData = cache.getCachedData('main');
+  if (cachedData) {
+    console.log("Returning cached data");
+    return cachedData;
+  } else {
+    console.log("No cached data available. Fetching new data...");
+    return await fetchDataAndUpdateCache(cache);
+  }
 }
 
 export { main, status, globalCache };
--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,15 +1,26 @@
 // @refresh reload
 import "virtual:uno.css";
-import { Component, Suspense, createSignal, createEffect, createResource, For } from "solid-js";
+import { Suspense, createSignal, createEffect, createResource, For } from "solid-js";
 import "./app.css";
 import MatrixRain from './Matrix';
 
-interface Candidate {
-  name?: string;
-  stash?: string;
+interface Validator {
+  address: string;
+  is1kv: boolean;
+  identity?: string;
+  nodeName?: string;
   email?: string;
   matrix?: string;
+  twitter?: string;
+  web?: string;
 }
+
+// Define the states
+const DisplayStates = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_1KV: "SHOW_1KV",
+  SHOW_NON_1KV: "SHOW_NON_1KV"
+};
 
 async function fetchData() {
   const apiUrl = process.env.API_URL || "http://localhost:4000/api"; // Fallback to localhost if not set
@@ -18,7 +29,8 @@ async function fetchData() {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    return await response.json();
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error("Failed to fetch data:", error);
     return null; // Return null in case of error
@@ -101,14 +113,30 @@ const BeefyStatusSlider = (props) => {
   );
 };
 
+
 export default function App() {
   const [data, { refetch: refetchData }] = createResource(fetchData);
   const [status, { refetch: refetchStatus }] = createResource(fetchStatus);
-  const [show1kv, setShow1kv] = createSignal(true);
+  const [displayState, setDisplayState] = createSignal(DisplayStates.SHOW_ALL);
   const [searchQuery, setSearchQuery] = createSignal("");
 
-  const count1kv = () => data()?.candidates1kv?.length || 0;
-  const countNon1kv = () => data()?.candidatesnon1kv?.length || 0;
+  // Update counting logic to consider the is1kv boolean field
+  const count1kv = () => data()?.validator?.filter(validator => validator.is1kv).length || 0;
+  const countNon1kv = () => data()?.validator?.filter(validator => !validator.is1kv).length || 0;
+
+  const handleToggleClick = () => {
+    setDisplayState(prevState => {
+      switch (prevState) {
+        case DisplayStates.SHOW_ALL:
+          return DisplayStates.SHOW_1KV;
+        case DisplayStates.SHOW_1KV:
+          return DisplayStates.SHOW_NON_1KV;
+        default:
+          return DisplayStates.SHOW_ALL;
+      }
+    });
+  };
+
 
   // Define the refresh interval for fetching data
   const refreshInterval = 60000; // 60 seconds
@@ -135,18 +163,42 @@ export default function App() {
     };
   });
 
-  const filteredCandidates = () => {
-    const candidates = show1kv() ? data()?.candidates1kv : data()?.candidatesnon1kv;
-    if (!candidates) return [];
 
-    const query = searchQuery().toLowerCase();
-    return candidates.filter((candidate: Candidate) => 
-      candidate.name?.toLowerCase().includes(query) ||
-        candidate.stash?.toLowerCase().includes(query) ||
-        (typeof candidate.matrix === 'string' && candidate.matrix.toLowerCase().includes(query)) ||
-        candidate.email?.toLowerCase().includes(query)
-    );
+  const getButtonLabel = () => {
+    switch (displayState()) {
+      case DisplayStates.SHOW_1KV:
+        return `1KV (${count1kv()})`;
+      case DisplayStates.SHOW_NON_1KV:
+        return `Non-1KV (${countNon1kv()})`;
+      default:
+        return "Show All";
+    }
   };
+
+  const filteredValidators = () => {
+    const validators = data()?.validator || [];
+    const query = searchQuery().toLowerCase();
+
+    return validators.filter(validator => {
+      // First, filter based on the display state
+      const displayFilter = (
+        displayState() === DisplayStates.SHOW_ALL ||
+          (displayState() === DisplayStates.SHOW_1KV && validator.is1kv) ||
+          (displayState() === DisplayStates.SHOW_NON_1KV && !validator.is1kv)
+      );
+
+      // Then, filter based on the search query
+      return displayFilter && (
+        validator.identity?.toLowerCase().includes(query) ||
+          validator.nodeName?.toLowerCase().includes(query) ||
+          validator.email?.toLowerCase().includes(query) ||
+          validator.matrix?.toLowerCase().includes(query) ||
+          validator.twitter?.toLowerCase().includes(query) ||
+          validator.web?.toLowerCase().includes(query)
+      );
+    });
+  };
+
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
@@ -173,31 +225,58 @@ export default function App() {
             />
             <button 
               class="w-2/10 px-4 py-2 bg-#D3FF33 bg-opacity-40 hover:bg-opacity-50 backdrop-blur hover:backdrop-blur text-white font-semibold rounded-lg shadow-md hover:bg-#56F39A focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 toggle" 
-              onClick={() => setShow1kv(!show1kv())}>
-              {show1kv() ? `1KV (${count1kv()})` : `Non-1KV (${countNon1kv()})`}
+              onClick={handleToggleClick}>
+              {getButtonLabel()}
             </button>
           </div>
           <div class="flex flex-col">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              <For each={filteredCandidates()}>
-                {candidate => (
-                  <div class="flex flex-col bg-blue-500 bg-opacity-30 backdrop-blur shadow-lg rounded-lg p-4 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl">
-                    <div class="flex items-center text-gray-100 mb-2">
-                      <i class="i-bi-person-fill mr-2"></i>
-                      <span>{candidate.name}</span>
-                    </div>
-                    <div class="flex items-center text-gray-100 mb-2">
-                      <i class="i-bi-wallet2 mr-2"></i>
-                      <span class="overflow-auto">{candidate.stash || candidate.address}</span>
-                    </div>
-                    <div class="flex items-center text-gray-100 mb-2">
-                      <i class="i-bi-chat-left-text-fill mr-2"></i>
-                      <span>{candidate.matrix}</span>
-                    </div>
-                    <div class="flex items-center text-gray-100">
-                      <i class="i-bi-envelope-fill mr-2"></i>
-                      <span>{candidate.email}</span>
-                    </div>
+              <For each={filteredValidators()}>
+                {validator => (
+                  <div class="flex flex-col bg-blue-500 bg-opacity-30 backdrop-blur shadow-lg rounded-lg p-4 transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl relative">
+                    {validator.identity && (
+                      <div class="flex items-center text-gray-100 mb-2">
+                        <i class="i-bi-person-fill mr-2"></i>
+                        <span>{validator.identity}</span>
+                        {validator.is1kv && <span class="text-xs px-2 py-1 bg-pink-800 shadow rounded text-white absolute top-4 right-4 bg-opacity-50">1kv</span>}
+                      </div>
+                    )}
+                    {validator.nodeName && (
+                      <div class="flex items-center text-gray-100 mb-2">
+                        <i class="i-bi-hdd-stack mr-2"></i>
+                        <span>{validator.nodeName}</span>
+                      </div>
+                    )}
+                    {validator.address && (
+                      <div class="flex items-center text-gray-100 mb-2">
+                        <i class="i-bi-wallet2 mr-2"></i>
+                        <span class="overflow-hidden">{validator.address}</span>
+                      </div>
+                    )}
+                    {validator.matrix && (
+                      <div class="flex items-center text-gray-100 mb-2">
+                        <i class="i-bi-chat-left-text-fill mr-2"></i>
+                        <span>{validator.matrix}</span>
+                      </div>
+                    )}
+                    {validator.email && (
+                      <div class="flex items-center text-gray-100">
+                        <i class="i-bi-envelope-fill mr-2"></i>
+                        <span>{validator.email}</span>
+                      </div>
+                    )}
+                    {validator.twitter && (
+                      <div class="flex items-center text-gray-100 mt-2">
+                        <i class="i-bi-twitter mr-2"></i>
+                        <span>{validator.twitter}</span>
+                      </div>
+                    )}
+                    {validator.web && (
+                      <div class="flex items-center text-gray-100 mt-2">
+                        <i class="i-bi-globe mr-2"></i>
+                        <span>{validator.web}</span>
+                      </div>
+                    )}
                   </div>
                 )}
               </For>
